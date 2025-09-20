5aafa6db602ea2eb96c09a02adb7077f162ea063
edwardo37 <cameronschultz421@gmail.com>
2025-09-20 13:48:03 -0400

    Add description to Title

--- Models/Title.cs
+++ Models/Title.cs
@@ -6,6 +6,7 @@
     public class Title : EntityBase
     {
         public required string Name { get; set; }
+        public string? Description { get; set; }
         public string? ISBN { get; set; }
         public DateOnly PublishDate { get; set; }
         public required int AuthorId { get; set; }
