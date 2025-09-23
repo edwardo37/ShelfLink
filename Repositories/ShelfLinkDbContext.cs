71fd9f6cb94c7671f60228d1eace3be22da13f8a
edwardo37 <cameronschultz421@gmail.com>
2025-09-22 22:09:49 -0400

    Remove DbSets from DbContext, will be implemented in repos

--- Repositories/ShelfLinkDbContext.cs
+++ Repositories/ShelfLinkDbContext.cs
@@ -6,18 +6,5 @@ namespace ShelfLink.Repositories
     public class ShelfLinkDbContext : DbContext
     {
         public ShelfLinkDbContext(DbContextOptions<ShelfLinkDbContext> options) : base(options) { }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserRole> Roles { get; set; }
-        public DbSet<UserStatus> Statuses { get; set; }
-
-
-        public DbSet<Title> Titles { get; set; }
-        public DbSet<TitleAuthor> Authors { get; set; }
-        public DbSet<TitlePublisher> Publishers { get; set; }
-        public DbSet<TitleGenre> Genres { get; set; }
-        public DbSet<TitleCategory> Categories { get; set; }
-        public DbSet<TitleCopy> Copies { get; set; }
-        public DbSet<TitleHold> Holds { get; set; }
     }
 }
