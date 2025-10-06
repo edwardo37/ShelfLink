7e3258edace030af8018ec1bc4264667ed3a7d15
edwardo37 <cameronschultz421@gmail.com>
2025-10-06 18:27:19 -0400

    Add repositories to Title service

---
+++ Services/TitleService.cs
@@ -0,0 +1,40 @@
+﻿using ShelfLink.Repositories;
+using ShelfLink.Models;
+
+namespace ShelfLink.Services
+{
+    public class TitleService
+    {
+        private readonly IShelfLinkRepository<Title> _titleRepo;
+        private readonly IShelfLinkRepository<TitleAuthor> _authorRepo;
+
+        private readonly IShelfLinkRepository<TitlePublisher> _publisherRepo;
+        private readonly IShelfLinkRepository<TitleGenre> _genreRepo;
+        private readonly IShelfLinkRepository<TitleCategory> _categoryRepo;
+
+        private readonly IShelfLinkRepository<TitleCopy> _copyRepo;
+        private readonly IShelfLinkRepository<CopyCheckout> _checkoutRepo;
+        private readonly IShelfLinkRepository<TitleHold> _holdRepo;
+
+        public TitleService(
+            IShelfLinkRepository<Title> titleRepo,
+            IShelfLinkRepository<TitleAuthor> authorRepo,
+            IShelfLinkRepository<TitlePublisher> publisherRepo,
+            IShelfLinkRepository<TitleGenre> genreRepo,
+            IShelfLinkRepository<TitleCategory> categoryRepo,
+            IShelfLinkRepository<TitleCopy> copyRepo,
+            IShelfLinkRepository<CopyCheckout> checkoutRepo,
+            IShelfLinkRepository<TitleHold> holdRepo
+        )
+        {
+            _titleRepo = titleRepo;
+            _authorRepo = authorRepo;
+            _publisherRepo = publisherRepo;
+            _genreRepo = genreRepo;
+            _categoryRepo = categoryRepo;
+            _copyRepo = copyRepo;
+            _checkoutRepo = checkoutRepo;
+            _holdRepo = holdRepo;
+        }
+    }
+}
