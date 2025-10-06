592aa72d4ca18931802eb65a3a93f4618b4b0485
edwardo37 <cameronschultz421@gmail.com>
2025-10-06 18:55:00 -0400

    Remove nullables from empty strings in TitleFilterRequest

--- DTOs/TitleFilterRequest.cs
+++ DTOs/TitleFilterRequest.cs
@@ -5,13 +5,13 @@ namespace ShelfLink.DTOs
     public class TitleFilterRequest
     {
         [Length(1, 200)]
-        public string? Name { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         [Length(10, 13)]
-        public string? ISBN { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
         public DateOnly? PublishDate { get; set; } = null;
-        public string? Author { get; set; } = string.Empty;
-        public string? Publisher { get; set; } = string.Empty;
-        public string? Genre { get; set; } = string.Empty;
-        public string? Category { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
     }
 }
