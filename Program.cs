41f133b78a304fe51dac6545d1949ea2fb1a2647
edwardo37 <cameronschultz421@gmail.com>
2025-10-07 20:28:51 -0400

    Remove all other repos until needed

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ShelfLinkDbContext>(options =>
+builder.Services.AddDbContext<DbContext>(options =>
     options.UseSqlServer(Environment.GetEnvironmentVariable("SqlServerConnection"))
 );
 
--- Services/TitleService.cs
+++ Services/TitleService.cs
@@ -7,35 +7,12 @@ namespace ShelfLink.Services
     public class TitleService
     {
         private readonly IShelfLinkRepository<Title> _titleRepo;
-        private readonly IShelfLinkRepository<TitleAuthor> _authorRepo;
-
-        private readonly IShelfLinkRepository<TitlePublisher> _publisherRepo;
-        private readonly IShelfLinkRepository<TitleGenre> _genreRepo;
-        private readonly IShelfLinkRepository<TitleCategory> _categoryRepo;
-
-        private readonly IShelfLinkRepository<TitleCopy> _copyRepo;
-        private readonly IShelfLinkRepository<CopyCheckout> _checkoutRepo;
-        private readonly IShelfLinkRepository<TitleHold> _holdRepo;
 
         public TitleService(
-            IShelfLinkRepository<Title> titleRepo,
-            IShelfLinkRepository<TitleAuthor> authorRepo,
-            IShelfLinkRepository<TitlePublisher> publisherRepo,
-            IShelfLinkRepository<TitleGenre> genreRepo,
-            IShelfLinkRepository<TitleCategory> categoryRepo,
-            IShelfLinkRepository<TitleCopy> copyRepo,
-            IShelfLinkRepository<CopyCheckout> checkoutRepo,
-            IShelfLinkRepository<TitleHold> holdRepo
+            IShelfLinkRepository<Title> titleRepo
         )
         {
             _titleRepo = titleRepo;
-            _authorRepo = authorRepo;
-            _publisherRepo = publisherRepo;
-            _genreRepo = genreRepo;
-            _categoryRepo = categoryRepo;
-            _copyRepo = copyRepo;
-            _checkoutRepo = checkoutRepo;
-            _holdRepo = holdRepo;
         }
 
 
