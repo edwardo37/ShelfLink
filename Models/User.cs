923158454f6d57d2b7be431eb37158939e504de3
edwardo37 <cameronschultz421@gmail.com>
2025-10-15 18:39:22 -0400

    Add Fine nav property to User

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,14 @@
-﻿namespace ShelfLink.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShelfLink.Models
 {
     /// <summary>
     /// Library member class
     /// </summary>
+    [Index(nameof(Email), IsUnique=true)]
     public class User : EntityBase
     {
+        public int UserId { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public string? Email { get; set; }
@@ -13,6 +17,7 @@
         public required UserStatus Status { get; set; }
         public required UserRole Role { get; set; }
         // Navigation properties
+        public UserFine? Fine { get; set; }
         public List<CopyCheckout> Checkouts { get; } = new List<CopyCheckout>();
         public List<TitleHold> Holds { get; } = new List<TitleHold>();
     }
