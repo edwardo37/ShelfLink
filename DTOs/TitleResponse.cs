1c746f3036fa56b7dbc123755edb510120db1827
edwardo37 <cameronschultz421@gmail.com>
2025-10-15 20:43:53 -0400

    Rename to TitleId

--- DTOs/TitleResponse.cs
+++ DTOs/TitleResponse.cs
@@ -2,7 +2,7 @@
 {
     public class TitleResponse
     {
-        public int Id { get; set; }
+        public int TitleId { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; } = "";
         public string? ISBN { get; set; }
