ce8669a4cab67da03ebbc19d0bf018dbcf645d16
edwardo37 <cameronschultz421@gmail.com>
2025-10-26 11:20:47 -0400

    Make TitleResponse reference AuthorResponse object, not fields

--- DTOs/TitleResponse.cs
+++ DTOs/TitleResponse.cs
@@ -7,8 +7,7 @@
         public string? Description { get; set; } = "";
         public string? ISBN { get; set; }
         public DateOnly PublishDate { get; set; }
-        public List<int>? AuthorIds { get; set; } = [];
-        public List<string>? AuthorNames { get; set; } = [];
+        public List<AuthorResponse> Authors { get; set; } = new List<AuthorResponse>();
         public string? Publisher { get; set; } = "";
         public string? Genre { get; set; } = "";
         public string CategoryName { get; set; } = string.Empty;
